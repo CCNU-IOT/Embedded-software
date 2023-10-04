44433924aa8132b9f48065b763ad96b5db89b7e5
undefined-0 <3047423259@qq.com>
2023-10-04 17:21:22 +0800

    修改LED呼吸灯注释

--- STM32_Projects/STM32F103ZET6/Templete_zet/Drivers/BSP/Src/led.c
+++ STM32_Projects/STM32F103ZET6/Templete_zet/Drivers/BSP/Src/led.c
@@ -38,15 +38,12 @@ void breath_led_init(void)
 }
 
 /**
-  * @brief  Set the TIM Capture Compare Register value on runtime without calling another time ConfigChannel function.
-  * @param  __HANDLE__ TIM handle.
-  * @param  __CHANNEL__ TIM Channels to be configured.
-  *          This parameter can be one of the following values:
-  *            @arg TIM_CHANNEL_1: TIM Channel 1 selected
-  *            @arg TIM_CHANNEL_2: TIM Channel 2 selected
-  *            @arg TIM_CHANNEL_3: TIM Channel 3 selected
-  *            @arg TIM_CHANNEL_4: TIM Channel 4 selected
-  * @param  __COMPARE__ specifies the Capture Compare register new value.
+  * @brief  实现LED呼吸灯。
+  * @param  direction: 呼吸方向。
+  *            该参数可取的值如下：
+  *            @arg Breath_Positive: 呼吸方向为正向。
+  *            @arg Breath_Positive: 呼吸方向为负向。
+  * @param  duty: 占空比。
   * @retval None
   */
 void breath_led_brightness(Breath_LED_Direction *direction, uint32_t *duty)
@@ -57,9 +54,10 @@ void breath_led_brightness(Breath_LED_Direction *direction, uint32_t *duty)
     // 如果呼吸方向为负向（Breath_Negative），则减小占空比
     else if (*direction == Breath_Negative)
       (*duty)--;
-
+	  // 如果占空比超过阈值（PWM_GENERAL_TIM_ARR），则将呼吸方向设置为负向
     if (*duty >= BREATH_LED_PWM_GENERAL_TIM_ARR)
       *direction = Breath_Negative; 
+    // 如果占空比为零，则将呼吸方向设置为正向
     else if (!(*duty))
       *direction = Breath_Positive; 
     __HAL_TIM_SET_COMPARE(&breath_led_pwm_time_handle, BREATH_LED_PWM_GRNERAL_TIM_CHANNEL2, *duty);
