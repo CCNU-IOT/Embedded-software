4d0ec367e33d87bd155b21b4c61d733865a01a0a
nickaljy <1031964705@qq.com>
2023-10-12 21:49:36 +0800

    UART驱动代码部分实现

--- STM32_Projects/STM32F103ZET6/Templete_zet/Drivers/BSP/Inc/uart.h
+++ STM32_Projects/STM32F103ZET6/Templete_zet/Drivers/BSP/Inc/uart.h
@@ -2,4 +2,10 @@
 #define __UART_H__
 #include "main.h"
 
+#define UART_DEBUG_BAUDRATE                                 115200U
+#define UART_DEBUG                                          USART1
+#define UART_DEBUG_TX_GPIO_PORT                             GPIOA
+#define UART_DEBUG_TX_GPIO_PIN                              GPIO_PIN_9
+#define UART_DEBUG_RX_GPIO_PORT                             GPIOA
+#define UART_DEBUG_RX_GPIO_PIN                              GPIO_PIN_10
 #endif
