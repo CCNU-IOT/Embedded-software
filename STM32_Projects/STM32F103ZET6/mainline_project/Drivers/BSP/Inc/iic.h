6787dc19be94cc40757901afbc0054399272b281
undefined-0 <3047423259@qq.com>
2023-10-15 10:39:26 +0800

    Update iic.h

--- STM32_Projects/STM32F103ZET6/mainline_project/Drivers/BSP/Inc/iic.h
+++ STM32_Projects/STM32F103ZET6/mainline_project/Drivers/BSP/Inc/iic.h
@@ -3,8 +3,8 @@
 #include "main.h"
 
 void I2C_Init(void);
-void I2C_set_SCL(i);
-void I2C_set_SDA(i);
+void I2C_set_SCL(uint_8);
+void I2C_set_SDA(uint_8);
 void I2C_Start(void);
 void I2C_Stop(void);
 HAL_StatusTypeDef I2C_SendData(uint8_t address, uint8_t* data, uint16_t size);
